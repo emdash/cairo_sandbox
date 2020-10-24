ec1ff8a8c26b4e526e7386c09fcc6f0e702af3ff
emdash <dotsony@gmail.com>
2020-10-24 16:20:44 -0700

    Parameter names should be valid shell identifiers.

--- examples/tach.py
+++ examples/tach.py
@@ -1,25 +1,26 @@
 if __name__ == "init":
     import math
-    params.define("arc-length", Angle(3 * math.pi / 2))
-    params.define("min-rpm", Numeric(0, 20000, 1, 0))
-    params.define("max-rpm", Numeric(0, 20000, 1, 6500))
+    params.define("arc_length", Angle(3 * math.pi / 2))
+    params.define("min_rpm", Numeric(0, 20000, 1, 0))
+    params.define("max_rpm", Numeric(0, 20000, 1, 6500))
     params.define("redline", Numeric(0, 20000, 1, 6000))
     params.define("ticks", Numeric(1, 5000, 1, 500))
-    params.define("hub-radius", Numeric(2, 100, 1, 11))
-    params.define("number-radius", Numeric(0, 1, 1/256.0, 0.65))
-    params.define("tick-radius", Numeric(0, 1, 1/256.0, 0.80))
-    params.define("needle-radius", Numeric(0, 1, 1/256.0, 0.85))
-    params.define("tick-length", Numeric(0, 1, 1/256.0, 0.125))
+    params.define("hub_radius", Numeric(2, 100, 1, 11))
+    params.define("number_radius", Numeric(0, 1, 1/256.0, 0.65))
+    params.define("tick_radius", Numeric(0, 1, 1/256.0, 0.80))
+    params.define("needle_radius", Numeric(0, 1, 1/256.0, 0.85))
+    params.define("tick_length", Numeric(0, 1, 1/256.0, 0.125))
     params.define("rpm", Numeric(0, 6500, 1, 1000))
-    params.define("show-outline", Toggle(True))
-    params.define("radial-numbers", Toggle(True))
-    params.define("dial-color", Color(0, 0, 0))
-    params.define("needle-color", Color(1, 0, 0))
+    params.define("show_outline", Toggle(True))
+    params.define("radial_numbers", Toggle(True))
+    params.define("background_color", Color(1, 1, 1))
+    params.define("dial_color", Color(0, 0, 0))
+    params.define("needle_color", Color(1, 0, 0))
 else:
-    arc_length = params["arc-length"]
+    arc_length = params["arc_length"]
     arc_remainder = 2 * math.pi - arc_length
-    min_rpm = params["min-rpm"]
-    max_rpm = params["max-rpm"]
+    min_rpm = params["min_rpm"]
+    max_rpm = params["max_rpm"]
     redline = params["redline"]
     ticks = params["ticks"]
     start = arc_remainder / 2 + math.pi / 2
@@ -30,11 +31,13 @@ else:
         percent = rpm / rpm_range
         return percent * arc_length + start
 
-    cr.set_source(params["dial-color"])
+    cr.set_source(params["background_color"])
+    cr.paint()
+    cr.set_source(params["dial_color"])
 
     with helpers.box(window.inset(5), clip=False) as bounds:
         radius = min(bounds.width, bounds.height) * 0.5
-        hub_radius = max(5, radius / params["hub-radius"])
+        hub_radius = max(5, radius / params["hub_radius"])
 
         cr.set_line_width(5)
         cr.set_line_cap(cairo.LineCap.ROUND)
@@ -42,10 +45,10 @@ else:
             with helpers.save():
                 angle = rpm_to_angle(tick)
                 cr.rotate(angle)
-                cr.move_to(radius * params["tick-radius"], 0)
-                cr.line_to(radius * params["tick-radius"] + radius * params["tick-length"], 0)
-                cr.translate(radius * params["number-radius"], 0)
-                if params["radial-numbers"]:
+                cr.move_to(radius * params["tick_radius"], 0)
+                cr.line_to(radius * params["tick_radius"] + radius * params["tick_length"], 0)
+                cr.translate(radius * params["number_radius"], 0)
+                if params["radial_numbers"]:
                     cr.rotate(math.pi / 2)
                 else:
                     cr.rotate(-angle)
@@ -55,17 +58,17 @@ else:
 
         cr.stroke()
 
-        if params["show-outline"]:
+        if params["show_outline"]:
             cr.translate(*bounds.center)
             cr.arc(0, 0, radius, start, end)
             cr.close_path()
             cr.stroke()
 
-        cr.set_source(params["needle-color"])
+        cr.set_source(params["needle_color"])
 
         with helpers.save():
             cr.rotate(rpm_to_angle(params["rpm"]))
-            cr.move_to(radius * params["needle-radius"], 0)
+            cr.move_to(radius * params["needle_radius"], 0)
             cr.line_to(0, hub_radius)
             cr.line_to(0, -hub_radius)
             cr.close_path()
